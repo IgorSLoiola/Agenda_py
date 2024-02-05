7553037d3f7c8ccd0f82badd93a820f750c926ed
igor <igaumloiola@gmail.com>
2024-02-05 07:56:33 -0300

    second-commit

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from contact import models
 
 # Create your views here.
 def index(request):
+    contacts = models.Contact.objects.all()
+
+    context = {
+        'contacts' : contacts,
+    }
     return render(request,
                   'contact/pages/index.html',
+                  context,
                   )
